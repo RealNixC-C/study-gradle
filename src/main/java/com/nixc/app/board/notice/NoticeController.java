de69aa00bfc1f9a1ed4593a20c785fd3080651d5
RealNixC-C <john7622@naver.com>
2025-08-28 17:59:04 +0900

    CorseConfig 추가

--- src/main/java/com/nixc/app/board/notice/NoticeController.java
+++ src/main/java/com/nixc/app/board/notice/NoticeController.java
@@ -3,6 +3,7 @@ package com.nixc.app.board.notice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/notice/**")
 public class NoticeController {
 
